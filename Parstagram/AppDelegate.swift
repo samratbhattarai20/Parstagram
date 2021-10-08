c4305895553f48365a450734bc64013e924ed234
samratbhattarai20 <archanbhattarai@gmail.com>
2021-10-07 23:40:48 -0500

    Done with the required tasks for the assignment part but there's a bug that needs to be solved

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "ZC5j32CVM5dL8JLoLvMpirb69R2AkTbJPV5XNzN7"
+            $0.clientKey = "Y4fZI8AX5HNJOgQdDdG5oJFOPLVBRaoWz1OyXW3A"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
